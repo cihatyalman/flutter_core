d8ae36d32645206ad2e567dc43246c74f2ede00f
Cihat Yalman <38735070+cihatyalman@users.noreply.github.com>
2021-04-18 03:01:05 +0300

    Add files via upload

---
+++ Core/api_service/abstract/i_api_service.dart
@@ -0,0 +1,6 @@
+class IApiService{
+  // ignore: missing_return
+  Future postData(Map json){}
+  // ignore: missing_return
+  Future getData(){}
+}
---
+++ Core/api_service/concrete/api_service.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+
+import '../abstract/i_api_service.dart';
+import 'package:http/http.dart' as http;
+
+class ApiService implements IApiService{
+  // Uri(scheme: "https",host: "example.com",path: "/other")
+  final Uri _uri;
+  ApiService(this._uri);
+
+  final Map<String,String> _headers = <String, String>{'Content-Type': 'application/json; charset=UTF-8'};
+
+  @override
+  Future getData() async{
+    http.Response response = await http.get(_uri,headers: _headers);
+    return jsonDecode(response.body);
+  }
+
+  @override
+  Future postData(Map json) async{
+    http.Response response = await http.post(_uri,headers: _headers,body: jsonEncode(json));
+    return jsonDecode(response.body);
+  }
+
+
+}
