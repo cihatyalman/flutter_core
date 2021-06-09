31a73d81de94caa31a3c2baa80b74728e19abaa8
Cihat Yalman <38735070+cihatyalman@users.noreply.github.com>
2021-06-09 19:40:16 +0300

    Add files via upload

---
+++ Core/utilities/helper_functions.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+final HelperFunctions helperFunctions = HelperFunctions();
+class HelperFunctions{
+
+  Future delay([int millisecond = 1000]) async{
+    await Future.delayed(Duration(milliseconds: millisecond));
+  }
+
+  // #region Get 
+  String getTimestamp([DateTime date]){
+    date ??= DateTime.now();
+    return date.millisecondsSinceEpoch.toString().
+      substring(0,date.millisecondsSinceEpoch.toString().length-3);
+  }
+
+  DateTime getDateTime(String timestamp){
+    return DateTime.fromMillisecondsSinceEpoch(int.tryParse(timestamp)*1000);
+  }
+  // #endregion
+
+  // #region Show 
+  showSnackBar(BuildContext context, String text,[int millisecond=3000]){
+    final snackBar = SnackBar(content: Text(text), duration: Duration(milliseconds: millisecond),);
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+  // #endregion
+
+}
