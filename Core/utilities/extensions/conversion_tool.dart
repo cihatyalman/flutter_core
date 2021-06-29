b260863646d061f7dd016b89e07a7a89798a5300
Cihat Yalman <38735070+cihatyalman@users.noreply.github.com>
2021-06-29 22:46:27 +0300

    Update conversion_tool.dart

--- Core/utilities/extensions/conversion_tool.dart
+++ Core/utilities/extensions/conversion_tool.dart
@@ -4,23 +4,39 @@ import 'dart:typed_data';
 import '../../entities/i_base_entity.dart';
 import 'package:flutter/material.dart';
 
-extension StringExtension on String{
+extension StringExtension on String {
   Uint8List get toBytesFromString => Uint8List.fromList(utf8.encode(this));
-  Uint8List get toBytesFromListString {
-    List<String> stringList = this.substring(1,this.length-1).split(",");
-    List<int> intList = List.generate(stringList.length, (index) => int.tryParse(stringList[index]));
-    return Uint8List.fromList(intList);
-  }
+  Uint8List get toBytesFromBase64 => base64Decode(this);
+
   String get toEnglishAlphabet => this
-    .replaceAll("ç", "c").replaceAll("ğ", "g").replaceAll("ı", "i")
-    .replaceAll("ö", "o").replaceAll("ş", "s").replaceAll("ü", "u");
+      .replaceAll("ç", "c")
+      .replaceAll("ğ", "g")
+      .replaceAll("ı", "i")
+      .replaceAll("ö", "o")
+      .replaceAll("ş", "s")
+      .replaceAll("ü", "u");
 }
 
-extension Uint8ListExtension on Uint8List{
+extension Uint8ListExtension on Uint8List {
   Image get toImageFromBytes => Image.memory(this);
   String get toStringFromBytes => utf8.decode(this);
+  String get toBase64FromBytes => base64Encode(this);
 }
 
-extension ListExtension on List<IBaseEntity>{
-  List<Map> get toMapList => List.generate(this.length, (index) => this[index].toMap());
+extension ListExtension on List<IBaseEntity> {
+  List<Map> get toMapList =>
+      List.generate(this.length, (index) => this[index].toMap());
+}
+
+extension ColorExtension on Color {
+  Color changeRGB({int red, int green, int blue}) {
+    Color tempColor = this;
+    if (red != null && red >= 0 && red <= 255)
+      tempColor = tempColor.withRed(red);
+    if (green != null && green >= 0 && green <= 255)
+      tempColor = tempColor.withGreen(green);
+    if (blue != null && blue >= 0 && blue <= 255)
+      tempColor = tempColor.withBlue(blue);
+    return tempColor;
+  }
 }
