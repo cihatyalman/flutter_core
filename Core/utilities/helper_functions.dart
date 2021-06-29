1342656c3939254d68cca41d8e845094b3d6e191
Cihat Yalman <38735070+cihatyalman@users.noreply.github.com>
2021-06-29 22:47:03 +0300

    Update helper_functions.dart

--- Core/utilities/helper_functions.dart
+++ Core/utilities/helper_functions.dart
@@ -1,27 +1,31 @@
 import 'package:flutter/material.dart';
 
 final HelperFunctions helperFunctions = HelperFunctions();
-class HelperFunctions{
 
-  Future delay([int millisecond = 1000]) async{
+class HelperFunctions {
+  Future delay([int millisecond = 1000]) async {
     await Future.delayed(Duration(milliseconds: millisecond));
   }
 
-  // #region Get 
-  String getTimestamp([DateTime date]){
+  // #region Get
+  String getTimestamp([DateTime date]) {
     date ??= DateTime.now();
-    return date.millisecondsSinceEpoch.toString().
-      substring(0,date.millisecondsSinceEpoch.toString().length-3);
+    return date.millisecondsSinceEpoch
+        .toString()
+        .substring(0, date.millisecondsSinceEpoch.toString().length - 3);
   }
 
-  DateTime getDateTime(String timestamp){
-    return DateTime.fromMillisecondsSinceEpoch(int.tryParse(timestamp)*1000);
+  DateTime getDateTime(String timestamp) {
+    return DateTime.fromMillisecondsSinceEpoch(int.tryParse(timestamp) * 1000);
   }
   // #endregion
 
-  // #region Show 
-  showSnackBar(BuildContext context, String text,[int millisecond=3000]){
-    final snackBar = SnackBar(content: Text(text), duration: Duration(milliseconds: millisecond),);
+  // #region Show
+  showSnackBar(BuildContext context, String text, [int millisecond = 3000]) {
+    final snackBar = SnackBar(
+      content: Text(text),
+      duration: Duration(milliseconds: millisecond),
+    );
     ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
   // #endregion
