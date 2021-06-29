a4bf34c7e62c8beb82af7b439f6e45a1e7395446
Cihat Yalman <38735070+cihatyalman@users.noreply.github.com>
2021-06-29 23:14:30 +0300

    Delete Core/exceptions directory

--- Core/exceptions/result_exceptions/abstract/i_data_result.dart
+++
@@ -1,6 +0,0 @@
-import 'i_result.dart';
-
-class IDataResult<T> extends IResult{
-  // ignore: missing_return
-  T get data{}
-}
--- Core/exceptions/result_exceptions/abstract/i_result.dart
+++
@@ -1,6 +0,0 @@
-class IResult{
-  // ignore: missing_return
-  bool get success{}
-  // ignore: missing_return
-  String get message{}
-}
